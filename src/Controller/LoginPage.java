d094ab4b71cc7b7768b4257890a275fccef80fcf
Jty <1151978174@qq.com>
2021-03-30 11:08:20 +0800

    设计了两个判断函数，实现 LoginPage对于登录和注册用户输入的判断，使用alert框来弹出不同的错误警告

--- src/Controller/LoginPage.java
+++ src/Controller/LoginPage.java
@@ -1,92 +1,124 @@
-
 package Controller;
 
+import NetBeans.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class LoginPage {
+    @FXML
+    private TextField idInput;
 
     @FXML
     private PasswordField pwInput;
 
-    @FXML
-    private Button loginBtn;
-
-    @FXML
-    private Button loginBtn1;
-
-    @FXML
-    private TextField idInput;
 
     @FXML
     private TextField regUserName;
 
     @FXML
-    private TextField idInput3;
+    private PasswordField regPw;
 
     @FXML
-    private TextField idInput2;
+    private PasswordField regPwConfirm;
 
-    @FXML
-    void onLoginBtnClicked(ActionEvent event) {
-
-
-    }
     /**
-    * @Description:  用来储存用户信息
+    * @Description: Login and register check ,check if the input string is correct.
     * @Param:
     * @return:
     * @Author: CloudKing
-    * @Date:
+    * @Date: 2021/3/30
     */
     @FXML
-    void onLoginBtn1Clicked(ActionEvent event) throws IOException {
+    void onLoginBtnClicked(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        String id = idInput.getText();
+        String pw = pwInput.getText();
+        if (new ValidChecker().isIDValid(id)) {
+            System.out.println("error");
 
-        String username= regUserName.getText();
-        String pwd=idInput2.getText();
+            alert.setTitle("Error");
+            alert.setHeaderText("Login failed");
+            alert.setContentText("Failed reason : ID is incorrect");
+            alert.show();
+        }
 
-        System.out.println(username);
-        System.out.println(pwd);
-        userpagecontrol.go_to_userpage();
+        if (new ValidChecker().isPwValid(pw)) {
+
+            alert.setTitle("Error");
+            alert.setHeaderText("Login failed");
+            alert.setContentText("Failed reason : Password is incorrect");
+            alert.show();
+        }
+
+        // Instantiate a new user, set the id and pw for it.
+        User user;
+
+        if (new ValidChecker().isValidAccount(id, pw)) {
+            //todo 此处getUser（），从hashmap中获得有特定id的用户。该函数打算写在User类中（或未来新建的ObjectGetter类）
+        }
 
 
+        //todo 此处将此user对象传递给检查存在函数（外部class），看是否存在该用户。不存在则窗口提示，存在则继续
 
-
+        //todo 存在该用户，跳转到登陆后页面，并且将User对象传递给下一个页面的Controller（外部class，进入下一环节）
 
     }
 
+    @FXML
+    void onRegBtnClicked(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        ValidChecker Checker = new ValidChecker();
+        String id = regUserName.getText();
+        String pw1 = regPw.getText();
+        String pw2 = regPwConfirm.getText();
+
+
+        //todo 验证id， pw1是否合法（外部class内容，待完成）
+        //todo 检查pw1， pw2是否相同
+        //todo 合法则存储该对象到User中
+        if (Checker.isIDValid(id)) {
+            System.out.println("error");
+
+            alert.setTitle("Error");
+            alert.setHeaderText("Register failed");
+            alert.setContentText("Failed reason : ID is incorrect");
+            alert.show();
+        }
+
+        else {
+            if (Checker.isPwValid(pw1)) {
+
+                alert.setTitle("Error");
+                alert.setHeaderText("Register failed");
+                alert.setContentText("Failed reason : Password1 is incorrect");
+                alert.show();
+            }
+            else {
+                if (Checker.isPwValid(pw2)) {
+
+                    alert.setTitle("Error");
+                    alert.setHeaderText("Register failed");
+                    alert.setContentText("Failed reason : Password2 is incorrect");
+                    alert.show();
+                }
+                else {
+                    if (!Checker.isSameString(pw1, pw2)) {
+                        alert.setTitle("Error");
+                        alert.setHeaderText("Register failed");
+                        alert.setContentText("Failed reason : Password1 and Password2 is not same");
+                        alert.show();
+                    }
+                    else {
+                        System.out.println("Register success");
+                    }
+                }
+            }
+        }
+
+
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/Controller/ValidChecker.java
+++ src/Controller/ValidChecker.java
@@ -1,8 +1,9 @@
 package Controller;
 
 
-import java.security.PublicKey;
-import java.util.HashMap;
+import NetBeans.User;
+import javafx.scene.control.Alert;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,117 +15,49 @@ public class ValidChecker {
     //todo 所有需要检查的方法都写在这里，比如检查用户名、密码是否符合规范，等等。
 
 
-    /**
-     * @Description: check the validation of the input Id and Password
-     * @return: boolean
-     * @Author: CloudKing
-     * @Date: 2021/3/30
-     */
-    public boolean isInvalidID(String id) {
+
+    public boolean isIDValid(String id) {
         //todo 实现id检查逻辑，只能包括大小写字母和数字，长度在4-15位。
-        if (id.length() >= 4 && id.length() <= 15) {
+        if(id.length()>=4 && id.length()<=15) {
             String regex = "[ _`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！ @#￥%……&*（）——+|{}【】‘；：”“’。，、？]|\n|\r|\t";
             Pattern pattern = Pattern.compile(regex);
             Matcher hasSpecial = pattern.matcher(id);
-            if (!hasSpecial.find()) {
-                return hasSpecial.find();
-            } else {
+            if(hasSpecial.find()==false){
                 return hasSpecial.find();
             }
-        } else {
-            return true;
+            else {
+                return hasSpecial.find();
+            }
         }
+        else { return  true; }
     }
 
-    public boolean isInvalidPw(String pw) {
-        return isInvalidID(pw);
+    public boolean isPwValid(String pw) {
+        if(pw.length()>=4 && pw.length()<=15) {
+            String regex = "[ _`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！ @#￥%……&*（）——+|{}【】‘；：”“’。，、？]|\n|\r|\t";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher hasSpecial = pattern.matcher(pw);
+            if(hasSpecial.find()==false){
+                return hasSpecial.find();
+            }
+            else {
+                return hasSpecial.find();
+            }
+        }
+        else { return  true; }
     }
 
+    
 
-    public boolean isSameString(String s1, String s2) {
+    public boolean isSameString (String s1, String s2) {
         return s1.equals(s2);
     }
 
-
-    /**
-     * This method checks whether the account belongs to a valid user.
-     *
-     * @param id User ID
-     * @param pw User password
-     * @return Whether this account is valid
-     * @throws Exception
-     */
-    public boolean isValidAccount(String id, String pw) throws Exception {
-
-/*        // Instantiate an all-accounts hashmap, it contains all accounts.
-        HashMap accounts = new IOClass().readAllAccount();
-
-
-        // if there is no such ID, then false is returned.
-        if (!accounts.containsKey(id)){
-            return false;
-        }
-
-        // there is this ID, but the pw is wrong. false is returned.
-        if (!pw.equals(accounts.get(id))) {
-            return false;
-        }
-
-        return true;*/
-        return isAccountExists(new IOClass().userAccountFilePath, id, pw);
-    }
-
-
-    public boolean isValidAccount(String path, String id, String pw) throws Exception {
-        return isAccountExists(path, id, pw);
-    }
-
-
-
-    public boolean isAccountExists(String path, String id, String pw) throws Exception {
-
-        // Instantiate an all-accounts hashmap, it contains all accounts.
-        HashMap accounts = new IOClass().readAllAccount(path);
-
-
-        // if there is no such ID, then false is returned.
-        if (!accounts.containsKey(id)) {
-            return false;
-        }
-
-        // there is this ID, but the pw is wrong. false is returned.
-        if (!pw.equals(accounts.get(id))) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * This method checks whether this ID has already exists.
-     *
-     * @param id
-     * @return Whether this is an existed ID.
-     * @throws Exception
-     * @author Thomas Andon
-     */
-    public boolean checkIDExists(String id) throws Exception {
-        HashMap accounts = new IOClass().readAllAccount();
-        if (accounts.containsKey(id)) {
-            return true;
-        }
+    public boolean isValidAccount (String s1, String s2) {
+        //todo 如果有这个user就true
         return false;
     }
-
-    public boolean checkIDExists(String path, String id) throws Exception {
-        HashMap accounts = new IOClass().readAllAccount(path);
-        if (accounts.containsKey(id)) {
-            return true;
-        }
-        return false;
-    }
-
-
+    
 
 
 }
