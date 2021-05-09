d88bb22de6eff12a922253c2e207c4160b005df7
Jty <1151978174@qq.com>
2021-05-09 22:18:32 +0800

    优化了用户和教练的个人信息展示，但是没有使用固定布局，之后可以继续优化

--- src/Controller/CheckCoachProfile.java
+++ src/Controller/CheckCoachProfile.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
@@ -22,7 +23,22 @@ import java.util.List;
 public class CheckCoachProfile {
 
     @FXML
-    private TableView<Trainer> table;
+    private TextField Sex;
+
+    @FXML
+    private TextField Height;
+
+    @FXML
+    private TextField Phone;
+
+    @FXML
+    private TextField ID;
+
+    @FXML
+    private TextField Weight;
+
+    @FXML
+    private TextField Name;
     @FXML
     private Text coachMain;
     private Trainer trainer;
@@ -37,56 +53,25 @@ public class CheckCoachProfile {
     }
 
     private void readCoachProfile(String coachProfilePath, String trainerID) {
-        ObservableList<Trainer> clist= FXCollections.observableArrayList();
-        List<Trainer> list = new ArrayList<Trainer>();
         File file = new File(coachProfilePath +"/" + trainerID +".txt");
         try{
 
             BufferedReader textFile = new BufferedReader(new FileReader(file));
-            String lineDta = "";
-            int i=0;
-            //第三步：将文档的下一行数据赋值给lineData，并判断是否为空，若不为空则输出
-            while ((lineDta = textFile.readLine()) != null){
-                Trainer s = new Trainer();
-                s.setTrainerID(lineDta.split("#")[0]);
-                s.setGender(lineDta.split("#")[1]);
-                s.setHeight(lineDta.split("#")[2]);
-                s.setWeight((lineDta.split("#")[3]));
-                s.setTel((lineDta.split("#")[4]));
-                s.setName((lineDta.split("#")[5]));
-                list.add(s);
+            String lineDta = textFile.readLine();
+            ID.setText(lineDta.split("#")[0]);
+            Sex.setText(lineDta.split("#")[1]);
+            Height.setText(lineDta.split("#")[2]);
+            Weight.setText(lineDta.split("#")[3]);
+            Phone.setText(lineDta.split("#")[4]);
+            Name.setText(lineDta.split("#")[5]);
 
-            }
-            clist.addAll(list);
-            textFile.close();
+
         }catch (FileNotFoundException e){
             System.out.println("没有找到指定文件");
         }catch (IOException e){
             System.out.println("文件读写出错");
         }
-        table.setItems(clist);//将集合的值 存储到tableView里
-        TableColumn<Trainer, String> table_name= new TableColumn<Trainer, String>("Name");//创建TableColumn  列名为序号
-        TableColumn<Trainer, String> table_id= new TableColumn<Trainer, String>("ID");
-        TableColumn<Trainer, String> table_height= new TableColumn<Trainer, String>("Height");
-        TableColumn<Trainer, String> table_weight= new TableColumn<Trainer, String>("Weight");
-        TableColumn<Trainer, String> table_gender= new TableColumn<Trainer, String>("Gender");
-        TableColumn<Trainer, String> table_phone= new TableColumn<Trainer, String>("Phone");
-        /**
-         * 反射取值
-         */
-        table_name.setCellValueFactory(new PropertyValueFactory<Trainer,String>("name"));
-        table_id.setCellValueFactory(new PropertyValueFactory<Trainer,String>("trainerID"));
-        table_height.setCellValueFactory(new PropertyValueFactory<Trainer,String>("height"));
-        table_weight.setCellValueFactory(new PropertyValueFactory<Trainer,String>("weight"));
-        table_gender.setCellValueFactory(new PropertyValueFactory<Trainer,String>("gender"));
-        table_phone.setCellValueFactory(new PropertyValueFactory<Trainer,String>("Tel"));
 
-        table.getColumns().add(table_name);
-        table.getColumns().add(table_id);
-        table.getColumns().add(table_height);
-        table.getColumns().add(table_weight);
-        table.getColumns().add(table_gender);
-        table.getColumns().add(table_phone);
     }
 
     public void toMainPage(MouseEvent mouseEvent) throws IOException {
--- src/Controller/CheckUserProfile.java
+++ src/Controller/CheckUserProfile.java
@@ -11,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
@@ -23,10 +24,27 @@ import java.util.List;
 public class CheckUserProfile {
 
     @FXML
-    private TableView<User> table;
+    private TextField Sex;
+
+    @FXML
+    private TextField Height;
+
+    @FXML
+    private TextArea Level;
+
+    @FXML
+    private TextField ID;
 
     @FXML
     private Text userMain;
+
+    @FXML
+    private TextField Weight;
+
+    @FXML
+    private TextField Name;
+
+
     private User user;
     /**
     *@Description: User can check their information in this window
@@ -43,57 +61,25 @@ public class CheckUserProfile {
     }
 
     private void readUserProfile(String userProfilePath, String id) {
-        ObservableList<User> slist= FXCollections.observableArrayList();
-        List<User> list = new ArrayList<User>();
         //第一步：先获取csv文件的路径，通过BufferedReader类去读该路径中的文件
         File file = new File(userProfilePath +"/" + id +".txt");//only for test
         try{
             //第二步：从字符输入流读取文本，缓冲各个字符，从而实现字符、数组和行（文本的行数通过回车符来进行判定）的高效读取。
             BufferedReader textFile = new BufferedReader(new FileReader(file));
-            String lineDta = "";
-            int i=0;
-            //第三步：将文档的下一行数据赋值给lineData，并判断是否为空，若不为空则输出
-            while ((lineDta = textFile.readLine()) != null){
-                User s = new User();
-                s.setTrainerID(lineDta.split("#")[0]);
-                s.setGender(lineDta.split("#")[1]);
-                s.setHeight(lineDta.split("#")[2]);
-                s.setWeight((lineDta.split("#")[3]));
-                s.setLevel((lineDta.split("#")[4]));
-                s.setName((lineDta.split("#")[5]));
-                list.add(s);
+            String lineDta = textFile.readLine();
+            ID.setText(lineDta.split("#")[0]);
+            Sex.setText(lineDta.split("#")[1]);
+            Height.setText(lineDta.split("#")[2]);
+            Weight.setText(lineDta.split("#")[3]);
+            Level.setText(lineDta.split("#")[4]);
+            Name.setText(lineDta.split("#")[5]);
 
-            }
-            slist.addAll(list);
-            textFile.close();
         }catch (FileNotFoundException e){
             System.out.println("没有找到指定文件");
         }catch (IOException e){
             System.out.println("文件读写出错");
         }
-        table.setItems(slist);//将集合的值 存储到tableView里
-        TableColumn<User, String> table_name= new TableColumn<User, String>("Name");//创建TableColumn  列名为序号
-        TableColumn<User, String> table_id= new TableColumn<User, String>("ID");
-        TableColumn<User, String> table_height= new TableColumn<User, String>("Height");
-        TableColumn<User, String> table_weight= new TableColumn<User, String>("Weight");
-        TableColumn<User, String> table_gender= new TableColumn<User, String>("Gender");
-        TableColumn<User, String> table_level= new TableColumn<User, String>("Vip Level");
-        /**
-         * 反射取值
-         */
-        table_name.setCellValueFactory(new PropertyValueFactory<User,String>("name"));//相当于getid
-        table_id.setCellValueFactory(new PropertyValueFactory<User,String>("id"));
-        table_height.setCellValueFactory(new PropertyValueFactory<User,String>("height"));
-        table_weight.setCellValueFactory(new PropertyValueFactory<User,String>("weight"));
-        table_gender.setCellValueFactory(new PropertyValueFactory<User,String>("gender"));
-        table_level.setCellValueFactory(new PropertyValueFactory<User,String>("level"));
 
-        table.getColumns().add(table_name);
-        table.getColumns().add(table_id);
-        table.getColumns().add(table_height);
-        table.getColumns().add(table_weight);
-        table.getColumns().add(table_gender);
-        table.getColumns().add(table_level);
 
     }
 
