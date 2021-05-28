673fffb94a62281db91fef3bbfb76056b0d98b3e
ThomasAndon <513230863@qq.com>
2021-05-28 10:57:40 +0800

    检查分支

--- src/Controller/ValidCheckerTest.java
+++ src/Controller/ValidCheckerTest.java
@@ -14,21 +14,32 @@ class ValidCheckerTest {
         assertFalse(new ValidChecker().isInvalidID("1258ffege"));
         assertTrue(new ValidChecker().isInvalidID("123"));
         assertTrue(new ValidChecker().isInvalidID("123@"));
+        assertTrue(new ValidChecker().isInvalidID("12345678w!"));
+        assertTrue(new ValidChecker().isInvalidID("qwertyuiop123456"));
+        assertTrue(new ValidChecker().isInvalidID("qwer^"));
 
     }
 
     @org.junit.jupiter.api.Test
     void isInvalidPw() {
-
+        assertFalse(new ValidChecker().isInvalidPw("1234"));
+        assertFalse(new ValidChecker().isInvalidPw("qwer"));
+        assertFalse(new ValidChecker().isInvalidPw("12qw"));
+        assertFalse(new ValidChecker().isInvalidPw("1258ffege"));
+        assertTrue(new ValidChecker().isInvalidPw("123"));
+        assertTrue(new ValidChecker().isInvalidPw("123@"));
+        assertTrue(new ValidChecker().isInvalidPw("12345678w!"));
+        assertTrue(new ValidChecker().isInvalidPw("qwertyuiop123456"));
+        assertTrue(new ValidChecker().isInvalidPw("qwer^"));
     }
 
     @org.junit.jupiter.api.Test
     void isNameValid() {
-    }
-
-    @Test
-    void test(){
-
-
+        assertTrue(new ValidChecker().isNameValid("qwer"));
+        assertTrue(new ValidChecker().isNameValid("qwert"));
+        assertTrue(new ValidChecker().isNameValid("qwertyuiopq"));
+        assertFalse(new ValidChecker().isNameValid("qwertyu iopq"));
+        assertFalse(new ValidChecker().isNameValid("hey1"));
+        assertFalse(new ValidChecker().isNameValid("@qwe"));
     }
 }
